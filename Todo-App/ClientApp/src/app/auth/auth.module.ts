0166a0a80895018e1c26edae497dc28093e63eb3
Edwin <edwin_m_paz@hotmail.com>
2020-06-04 15:37:50 -0600

    Login component create

--- Todo-App/ClientApp/src/app/auth/auth.module.ts
+++ Todo-App/ClientApp/src/app/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [AuthComponent],
+  declarations: [AuthComponent, LoginComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
