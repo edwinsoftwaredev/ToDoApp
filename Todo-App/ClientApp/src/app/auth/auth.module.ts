e29c15138f44d3303db8f9262b5f17f6c7c334d3
Edwin <edwin_m_paz@hotmail.com>
2020-06-04 15:53:25 -0600

    Revert "Login component create"
    This reverts commit 0166a0a80895018e1c26edae497dc28093e63eb3.
    
    Signed-off-by: Edwin <edwin_m_paz@hotmail.com>

--- Todo-App/ClientApp/src/app/auth/auth.module.ts
+++ Todo-App/ClientApp/src/app/auth/auth.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
-import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [AuthComponent, LoginComponent],
+  declarations: [AuthComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
