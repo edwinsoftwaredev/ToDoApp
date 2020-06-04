4145d7d7db6d808b0a21e4486db456936ee339ba
Edwin <edwin_m_paz@hotmail.com>
2020-06-04 16:02:00 -0600

    Sign up component create

--- Todo-App/ClientApp/src/app/auth/auth.module.ts
+++ Todo-App/ClientApp/src/app/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 @NgModule({
-  declarations: [AuthComponent, SignInComponent],
+  declarations: [AuthComponent, SignInComponent, SignUpComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
