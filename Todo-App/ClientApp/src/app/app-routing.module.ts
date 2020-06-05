5f9d6ddbacad0780a791b466262213ef23f55c9b
Edwin <edwin_m_paz@hotmail.com>
2020-06-04 19:01:54 -0600

    adding not found component to router

--- Todo-App/ClientApp/src/app/app-routing.module.ts
+++ Todo-App/ClientApp/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '**',
+  component: NotFoundComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
