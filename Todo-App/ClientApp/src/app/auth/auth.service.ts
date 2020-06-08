2ad1e5739f0e4d096b12023605ba70932b4b8fdc
Edwin <edwin_m_paz@hotmail.com>
2020-06-07 23:36:55 -0600

    vimrc intial configuration

--- Todo-App/ClientApp/src/app/auth/auth.service.ts
+++ Todo-App/ClientApp/src/app/auth/auth.service.ts
@@ -5,5 +5,21 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
+  private _isUserSignIn = false;
+
   constructor() { }
+
+  get isUserSignIn(): boolean {
+    return this._isUserSignIn;
+  }
+
+  /**
+   * This function determines if the user is authenticated.
+   * @param force if false we directly return the user already authenticated
+   */
+  identifyUser(force?: boolean): Promise<User> {
+    if (!force) {
+      
+    }
+  }
 }
--- Todo-App/ClientApp/src/app/landing/landing.guard.ts
+++ Todo-App/ClientApp/src/app/landing/landing.guard.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LandingGuard implements CanActivate {
+
+  constructor(private authService: AuthService) { }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return true;
   }
-  
 }
