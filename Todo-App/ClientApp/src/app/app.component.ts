775d8374acd02886e543ae38138e572a6c1a9613
Edwin <edwin_m_paz@hotmail.com>
2020-07-01 20:24:23 -0600

    removing unnecesary dependecies

--- Todo-App/ClientApp/src/app/app.component.ts
+++ Todo-App/ClientApp/src/app/app.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    public title = 'todo-app';
+  public title = 'todo-app';
+
+  constructor() {
+    //
+  }
 }
 
