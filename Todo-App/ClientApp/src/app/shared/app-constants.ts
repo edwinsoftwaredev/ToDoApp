411b00f7a1358644039069348f6c3f38be3b8ddb
Edwin <edwin_m_paz@hotmail.com>
2020-07-01 20:33:13 -0600

    adding constants url to connect to server authentication

--- Todo-App/ClientApp/src/app/shared/app-constants.ts
+++ Todo-App/ClientApp/src/app/shared/app-constants.ts
@@ -1,4 +1,4 @@
-export const SERVER_API_URL = 'https://localhost:5100/';
+export const SERVER_API_URL = 'https://localhost:5001/';
 export const REDIRECT_URI = 'http://localhost:4200/auth/code';
 export const REDIRECT_LOGOUT = 'http://localhost:4200/logout';
 
