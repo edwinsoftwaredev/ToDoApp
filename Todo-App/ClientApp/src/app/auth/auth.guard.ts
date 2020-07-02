249126f86422af09d8d592ea0fb928fcaf4fdcb1
Edwin <edwin_m_paz@hotmail.com>
2020-07-01 21:07:03 -0600

    adding method in auth.guard to check if user can activate an auth component

--- Todo-App/ClientApp/src/app/auth/auth.guard.ts
+++ Todo-App/ClientApp/src/app/auth/auth.guard.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import {AuthService} from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService) {
+    //
+  }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    return this.checkAuthentication(state.url);
+  }
+
+  /**
+   * if user is not authenticated return true, so the component canActivate
+   * @param url: the url to redirect
+   */
+  private checkAuthentication(url: string): boolean {
+    if (this.authService.isLoggedIn()) {
+      return false;
+    }
+
     return true;
   }
 }
