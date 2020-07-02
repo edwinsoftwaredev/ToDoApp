c2ea6ceeed8b43f44c15947099c10d18d0048b02
Edwin <edwin_m_paz@hotmail.com>
2020-07-02 15:15:19 -0600

    Redirect url based on development mode or productionmode

--- Todo-App/ClientApp/src/app/shared/app-constants.ts
+++ Todo-App/ClientApp/src/app/shared/app-constants.ts
@@ -1,4 +1,6 @@
+import {environment} from 'src/environments/environment';
+
 export const SERVER_API_URL = 'https://localhost:5001/';
-export const REDIRECT_URI = 'http://localhost:4200/auth/code';
+export const REDIRECT_URI = environment.production ? 'http://localhost:4200/auth/code' : 'http://localhost:9876/auth/code';
 export const REDIRECT_LOGOUT = 'http://localhost:4200/logout';
 
