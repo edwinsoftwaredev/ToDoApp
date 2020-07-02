5e3ef542e014234b0f05af2057de91d902ec7d04
Edwin <edwin_m_paz@hotmail.com>
2020-07-01 20:28:47 -0600

    adding methods requiered for app authentication

--- Todo-App/ClientApp/src/app/auth/auth.service.ts
+++ Todo-App/ClientApp/src/app/auth/auth.service.ts
@@ -1,36 +1,80 @@
 import { Injectable } from '@angular/core';
-import {UserManager, UserManagerSettings, User as OidcUser} from 'oidc-client';
+import {UserManager, UserManagerSettings, User as OidcUser, Profile} from 'oidc-client';
 import {SERVER_API_URL, REDIRECT_URI, REDIRECT_LOGOUT} from '../shared/app-constants';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private _userManager: UserManager;
+  private _user: OidcUser = null;
+
   private _userManagerSettings: UserManagerSettings = {
-      authority: SERVER_API_URL,
-      client_id: 'TodoAppFirstPartyUser',
-      redirect_uri: REDIRECT_URI,
-      response_type: 'code',
-      scope: 'TodoAppUser OpenId Profile',
-      post_logout_redirect_uri: REDIRECT_LOGOUT
+    authority: SERVER_API_URL,
+    client_id: 'TodoAppFirstPartyUser',
+    redirect_uri: REDIRECT_URI,
+    response_type: 'code',
+    scope: 'TodoAppUser OpenId Profile',
+    post_logout_redirect_uri: REDIRECT_LOGOUT
   };
 
+  get User(): OidcUser {
+    return this._user;
+  }
+
+  private _userManager: UserManager = new UserManager(this._userManagerSettings);
+
   constructor() {
-      // UserManager requires a config object
-      this._userManager = new UserManager(this._userManagerSettings);
+    this.identifyUser();
   }
 
   /**
-   * This function determines if the user is authenticated.
+   * This function determines if the user is authenticated or logged in.
    * @param force: if false we directly return the user already authenticated
    */
-  identifyUser(force?: boolean): Promise<OidcUser> {
-    if (!force) {
-        this._userManager.removeUser();
-    }
+  isLoggedIn(): boolean {
+    return this._user != null && !this._user.expired;
+  }
 
-    return this._userManager.getUser();
+  /**
+   * Return the Oidc user instance profile
+   */
+  getClaims(): Profile {
+    return this._user.profile;
+  }
+
+  /**
+   * this function return the token with its type.
+   * this could be used to make api request
+   */
+  getAuthorizationHeaderValue(): string {
+    return `${this._user.token_type} ${this._user.access_token}`;
+  }
+
+  /**
+   * Redirect to the authorization endpoint
+   */
+  startAuthentication(): Promise<void> {
+    return this._userManager.signinRedirect()
+      .catch((error: HttpErrorResponse) => {
+        if (error.message === 'Network Error') {
+          //
+        }
+      });
+  }
+
+  /**
+   * Process the signin callback
+   */
+  async completeAuthentication(): Promise<void> {
+    const user = await this._userManager.signinRedirectCallback();
+    this._user = user;
+  }
+
+  private identifyUser(): void {
+    this._userManager.getUser().then((user: OidcUser) => {
+      this._user = user;
+    });
   }
 }
