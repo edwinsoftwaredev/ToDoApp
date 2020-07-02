1182c09431a0c96ec136ff44707787d819e84b19
Edwin <edwin_m_paz@hotmail.com>
2020-07-01 20:27:18 -0600

    adding shared.module to auth.module

--- Todo-App/ClientApp/src/app/auth/auth.module.ts
+++ Todo-App/ClientApp/src/app/auth/auth.module.ts
@@ -5,12 +5,13 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   declarations: [AuthComponent, SignInComponent, SignUpComponent],
   imports: [
     CommonModule,
+    SharedModule,
     AuthRoutingModule
   ]
 })
