6824db533c33b2c2cf5a26ad0c02ba114aa951e8
Edwin <edwin_m_paz@hotmail.com>
2020-07-01 20:31:24 -0600

    testing startAuthentication method in auth.service.ts

--- Todo-App/ClientApp/src/app/auth/auth.service.spec.ts
+++ Todo-App/ClientApp/src/app/auth/auth.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
@@ -12,5 +11,9 @@ describe('AuthService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('startAuthentication() should return a Promise', () => {
+    expect(service.startAuthentication()).toBeInstanceOf(Promise);
   });
 });
