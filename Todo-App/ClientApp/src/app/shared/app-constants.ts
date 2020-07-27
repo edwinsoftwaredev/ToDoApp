a3bf9f454d883b4b9e87deea90786a2f3a63a826
Edwin <edwin_m_paz@hotmail.com>
2020-07-27 12:42:57 -0600

    cleaning dir before installing react

--- Todo-App/ClientApp/src/app/shared/app-constants.ts
+++
@@ -1,6 +0,0 @@
-import {environment} from 'src/environments/environment';
-
-export const SERVER_API_URL = 'https://localhost:5001/';
-export const REDIRECT_URI = environment.production ? 'http://localhost:4200/auth/codes' : 'http://localhost:9876/auth/codes';
-export const REDIRECT_LOGOUT = 'http://localhost:4200/logout';
-
