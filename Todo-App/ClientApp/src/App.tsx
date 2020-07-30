8590a9646c7d6c3447d89eca5fcf666885132357
Edwin <edwin_m_paz@hotmail.com>
2020-07-30 17:32:27 -0600

    changing file types css -> scss

--- Todo-App/ClientApp/src/App.tsx
+++ Todo-App/ClientApp/src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 
 function App() {
   return (
