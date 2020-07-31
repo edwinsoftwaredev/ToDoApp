7a3e6b28b3925c20232badbcd8b6006b7a09a772
Edwin <edwin_m_paz@hotmail.com>
2020-07-30 18:26:56 -0600

    bad character git error

--- Todo-App/ClientApp/src/App.tsx
+++ Todo-App/ClientApp/src/App.tsx
@@ -7,26 +7,52 @@ import {
   Route,
   Link
 } from 'react-router-dom';
-
+import {SignIn} from './auth/sign-in/sign-in';
+import {SignUp} from './auth/sign-up/sign-up';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
+        </header>
+
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/sigin">Sign In</Link>
+              </li>
+              <li>
+                <Link to="/signup">Sign Up</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+      </div>
+
+      <Switch>
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
