1484db3c6d29c37fbda0542dbafa3ddda03d5051
Edwin <edwin_m_paz@hotmail.com>
2020-08-07 17:56:19 -0600

    Add initial config in jest.config.js

--- Todo-App/ClientApp/jest.config.js
+++ Todo-App/ClientApp/jest.config.js
@@ -1,4 +1,17 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  roots: [
+    '<rootDir>/src'
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ],
 };
