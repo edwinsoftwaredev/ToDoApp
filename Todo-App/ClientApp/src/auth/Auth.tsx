b37e2cb56af5f65e6ab8e2cc0c2b8874890e1f9f
Edwin <edwin_m_paz@hotmail.com>
2020-08-11 19:57:33 -0600

    Add SignIn conditional rendering

--- Todo-App/ClientApp/src/auth/Auth.tsx
+++ Todo-App/ClientApp/src/auth/Auth.tsx
@@ -2,11 +2,26 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import SignIn from './sign-in/SignIn';
 import SignUp from './sign-up/SignUp';
+import {AuthService} from './AuthService';
+import {useQuery} from '../shared/utils';
+
+/*
+ * returns SignIn if the url is valid otherwise ConnectingAuthorizationServer
+ **/
+const SignInComponent: React.FC = (): JSX.Element => {
+  const isValidUrl = AuthService.isSignInAuthorizationUrl(useQuery().get('returnUrl'));
+  const ConnectingAuthorizationServer = AuthService.ConnectingAuthorizationServer;
+  const authService = AuthService.getInstance();
+
+  if (!isValidUrl) authService.startAuthentication();
+
+  return isValidUrl ? <SignIn /> : <ConnectingAuthorizationServer />;
+}
 
 const Auth: React.FC = (): JSX.Element => {
   return (
     <Switch>
-      <Route path="/signin" children={<SignIn />} />
+      <Route path="/signin" children={<SignInComponent />} />
       <Route path="/signup">
         <SignUp />
       </Route>
