35e981fa31692b174bb92a3d77fe285b5c70a09c
Edwin <edwin_m_paz@hotmail.com>
2020-08-17 21:59:50 -0600

    Fix remarks

--- Todo-App/Model/Auth/VM/UserVM.cs
+++ Todo-App/Model/Auth/VM/UserVM.cs
@@ -1,12 +1,11 @@
 using System.ComponentModel.DataAnnotations;
 
-/// <remarks>
-/// This class is only used to create/register new users.
-/// Others actions use User class.
-/// </remarks>
 namespace Todo_App.Model.Auth.VM {
+    /// <remarks>
+    /// This class is only used to create/register new users.
+    /// Others actions use User class.
+    /// </remarks>
     public class UserVM : User {
-
         // have to check if there is a spec for characters in passwords
         [Required]
         [MinLength(6), MaxLength(60)]
