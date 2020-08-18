bc0dd53abac60811f71f5d5bb8e8852b3610dddd
Edwin <edwin_m_paz@hotmail.com>
2020-08-17 22:09:58 -0600

    Replace UserService by IUserService interface

--- Todo-App/Controllers/Auth/UserController.cs
+++ Todo-App/Controllers/Auth/UserController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Todo_App.Model.Auth;
 using Todo_App.Model.Auth.VM;
-using Todo_App.Services.Models;
+using Todo_App.Services.Models.Interfaces;
 
 namespace Todo_App.Controllers.Auth
 {
@@ -12,8 +12,8 @@ namespace Todo_App.Controllers.Auth
     [Authorize]
     public class UserController : ControllerBase
     {
-        private readonly UserService _userService;
-        public UserController(UserService userService)
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
         {
             this._userService = userService;
         }
@@ -23,7 +23,7 @@ namespace Todo_App.Controllers.Auth
         public async Task<IActionResult> CreateUser(UserVM userVM)
         {
             if (ModelState.IsValid) {
-                await this._userService.CreateUser(userVM as User, userVM.Password);
+                await this._userService.Create(userVM as User, userVM.Password);
 
                 return Ok();
             }
