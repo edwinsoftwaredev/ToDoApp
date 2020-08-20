55008d1f1d28dae6521f6ffdbc18a147b1156bd8
Edwin <edwin_m_paz@hotmail.com>
2020-08-20 17:12:33 -0600

    Add test case when ModelState is not valid

--- Todo-App.Tests/UnitTests/UserControllerTests.cs
+++ Todo-App.Tests/UnitTests/UserControllerTests.cs
@@ -11,18 +11,23 @@ namespace Todo_App.Tests.UnitTests
 {
     public class UserControllerTests
     {
-        /*[Fact]
-        public async Task UserPost_ThrowsError_StateNotValid()
+        [Fact]
+        public async Task CreateUser_StateNotValid_BadRequest()
         {
-            var userStateNotValid = new UserVM {};
-
             var mockUserService = new Mock<IUserService>();
-            mockUserService.Setup(userService => userService.Create(userStateNotValid as User, userStateNotValid.Password))
-                .Returns(() => Task.Factory.StartNew(() => {}));
+            mockUserService.Setup(userService => userService.Create(It.IsAny<User>(), It.IsAny<string>()))
+                .Returns(Task.CompletedTask) // it must return a completed task
+                .Verifiable();
 
             var userController = new UserController(mockUserService.Object);
+            // When a ModelError is added to the ModelState, the actual state of the model(UserVM)
+            // is not checked. So if the actual state is correct but it has been added a
+            // ModelError to ModelState a BadRequest or Error is returned or thrown by the controller.
+            userController.ModelState.AddModelError("Password", "Required");
+
+            var userStateNotValid = new UserVM {};
             var result = await userController.CreateUser(userStateNotValid);
-            Assert.IsType<BadRequestResult>(result);
-        }*/
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
     }
 }
