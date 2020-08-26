4353dc7f96f747c5e36e3610929df4bab16802d6
Edwin <edwin_m_paz@hotmail.com>
2020-08-26 13:15:30 -0600

    Add RoleConstants class

---
+++ Todo-App/Utils/Constants/RoleConstants.cs
@@ -0,0 +1,8 @@
+namespace Todo_App.Utils.Constants
+{
+    public static class RoleConstants
+    {
+        public const string ADMIN_ROLE = "ADMIN";
+        public const string USER_ROLE = "USER";
+    }
+}
