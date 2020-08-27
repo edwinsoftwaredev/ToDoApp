544e96700a037507a991f8cb8e14ccaee644504d
Edwin <edwin_m_paz@hotmail.com>
2020-08-26 21:09:30 -0600

    Add required services to seed the database at startup

--- Todo-App/Program.cs
+++ Todo-App/Program.cs
@@ -1,11 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Todo_App.DAL;
+using Todo_App.Model.Auth;
 
 namespace Todo_App
 {
@@ -13,7 +13,33 @@ namespace Todo_App
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        public static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<IdDbContext>();
+                    var roleManager = services.GetRequiredService<RoleManager<Role>>();
+                    // Add more services here if required when seeding the database
+
+                    DataSeedingContext.Initialize(context, roleManager);
+                }
+                catch(Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
