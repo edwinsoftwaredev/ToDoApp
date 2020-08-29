7915444b530b6c1951938fbd01a81b40687c58d4
Edwin <edwin_m_paz@hotmail.com>
2020-08-28 18:19:35 -0600

    Add a new database migration

---
+++ Todo-App/Migrations/20200829000757_AddUserName_Picture.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Todo_App.Migrations
+{
+    public partial class AddUserName_Picture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Picture",
+                table: "User",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Picture",
+                table: "User");
+        }
+    }
+}
