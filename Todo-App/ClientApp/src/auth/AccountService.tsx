ab89bedc20bb317dbab346df2a46e12cce1eba72
Edwin <edwin_m_paz@hotmail.com>
2020-08-31 13:34:53 -0600

    Add method documentation

--- Todo-App/ClientApp/src/auth/AccountService.tsx
+++ Todo-App/ClientApp/src/auth/AccountService.tsx
@@ -1,16 +1,17 @@
 import Axios from 'axios';
 
 export class AccountService {
-  /*
+  /**
    * Registers a new user
-   **/
+   * @return {Promise<void>}
+   */
   public static registerUser(userObj: object): Promise<void> {
     return Axios.post(`${process.env.REACT_APP_API_SERVER_URL}api/user`, userObj);
   }
 
-  /*
+  /**
    * Deletes a user. Only authenticated user are able to delete their own accout.
-   **/
+   */
   public static deleteUser(userName: string): Promise<void> {
     return Axios.delete(`${process.env.REACT_APP_API_SERVER_URL}api/user?id=${userName}`);
   }
