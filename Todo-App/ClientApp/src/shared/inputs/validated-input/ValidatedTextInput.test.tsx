ddb424c2124b18b5d9de7d56c39b91ee9b0497ed
Edwin <edwin_m_paz@hotmail.com>
2020-09-10 15:13:50 -0600

    Add test case for ValidatedInput when input is not valid

--- Todo-App/ClientApp/src/shared/inputs/validated-input/ValidatedTextInput.test.tsx
+++ Todo-App/ClientApp/src/shared/inputs/validated-input/ValidatedTextInput.test.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ValidatedTextInput, {IValidatedTextInput} from './ValidatedTextInput';
-import {render} from '@testing-library/react';
+import {render, fireEvent, screen} from '@testing-library/react';
+import pretty from 'pretty';
 
 describe('ValidatedTextInput component', () => {
+  beforeEach(() => {});
 
-  beforeEach(() => {
-  });
-
-  beforeAll(() => {
-  });
+  beforeAll(() => {});
 
   test('should render', () => {
     const mockProps = {
@@ -18,13 +16,39 @@ describe('ValidatedTextInput component', () => {
       others: {
         autoComplete: 'off',
         maxLength: 20,
-        minLength: 6
+        minLength: 6,
       },
-      value: (value: string) => {}
-    }
+      value: (value: string) => {},
+    };
 
-    render(
-      <ValidatedTextInput {...mockProps} />
-    );
+    render(<ValidatedTextInput {...mockProps} />);
+  });
+
+  test('should not be valid when isValid is set to false, message is setted and touched', () => {
+    const mockProps = {
+      isValid: false,
+      message: 'MOCKMESSAGEERROR',
+      name: 'Username',
+      others: {
+        autoComplete: 'off',
+        maxLength: 20,
+        minLength: 6,
+      },
+      value: (value: string) => {},
+    };
+
+    const {container} = render(<ValidatedTextInput {...mockProps} />);
+    const input = screen.getByRole('textbox') as HTMLInputElement;
+    fireEvent.change(input, {target: {value: 'TOUCHED'}});
+
+    expect(pretty(container.innerHTML)).toMatchInlineSnapshot(`
+      "<div class=\\"validated-input-text not-valid\\">
+        <div class=\\"message-bar\\">
+          <div class=\\"message\\">
+            <div>Username MOCKMESSAGEERROR</div>
+          </div>
+        </div><input class=\\"uk-input\\" type=\\"text\\" name=\\"Username\\" autocomplete=\\"off\\" maxlength=\\"20\\" minlength=\\"6\\">
+      </div>"
+    `);
   });
 });
