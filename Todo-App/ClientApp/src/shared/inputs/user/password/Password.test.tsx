d72736dd7ba33c0f217fe10fed4678845c80bce2
Edwin <edwin_m_paz@hotmail.com>
2020-09-12 17:40:57 -0600

    Add password validation and test cases for validation

--- Todo-App/ClientApp/src/shared/inputs/user/password/Password.test.tsx
+++ Todo-App/ClientApp/src/shared/inputs/user/password/Password.test.tsx
@@ -43,6 +43,105 @@ describe('Password component tests', () => {
     });
   });
 
+  test('should not return value and call setMessage when input is not at leat 6 chars long', () => {
+    let mockInput = 'NOTTHISVALUE';
+    render(
+      <Password password={(password: string) => mockInput = password} />
+    );
+
+    const input = screen.getByRole('textbox') as HTMLInputElement;
+    // input is not at least 6 chars long
+    fireEvent.change(input, {target: {value: 'NOTLO'}});
+    expect(mockSetMessage).toHaveBeenCalledTimes(1);
+    expect(mockSetMessage).not.toHaveBeenCalledWith('');
+    expect(mockInput).toBe('');
+  });
+
+  test('should not return value and call setMessage when input doesnt contain 1 Uppercase', () => {
+    let mockInput = 'NOTTHISVALUE';
+    render(
+      <Password password={(password: string) => mockInput = password} />
+    );
+
+    const input = screen.getByRole('textbox') as HTMLInputElement;
+    // input is not at least 6 chars long
+    fireEvent.change(input, {target: {value: 'nouppercase'}});
+    expect(mockSetMessage).toHaveBeenCalledTimes(1);
+    expect(mockSetMessage).not.toHaveBeenCalledWith('');
+    expect(mockInput).toBe('');
+  });
+
+
+  test('should not return value and call setMessage when input doesnt contain 1 Lowecase', () => {
+    let mockInput = 'NOTTHISVALUE';
+    render(
+      <Password password={(password: string) => mockInput = password} />
+    );
+
+    const input = screen.getByRole('textbox') as HTMLInputElement;
+    // input is not at least 6 chars long
+    fireEvent.change(input, {target: {value: 'NOLOWERCASE'}});
+    expect(mockSetMessage).toHaveBeenCalledTimes(1);
+    expect(mockSetMessage).not.toHaveBeenCalledWith('');
+    expect(mockInput).toBe('');
+  });
+
+  test('should not return value and call setMessage when input doesnt contain 1 Special char', () => {
+    let mockInput = 'NOTTHISVALUE';
+    render(
+      <Password password={(password: string) => mockInput = password} />
+    );
+
+    const input = screen.getByRole('textbox') as HTMLInputElement;
+    // input is not at least 6 chars long
+    fireEvent.change(input, {target: {value: 'NoSpecialChar'}});
+    expect(mockSetMessage).toHaveBeenCalledTimes(1);
+    expect(mockSetMessage).not.toHaveBeenCalledWith('');
+    expect(mockInput).toBe('');
+  });
+
+  test('should not return value and call setMessage when input doesnt contain 1 number', () => {
+    let mockInput = 'NOTTHISVALUE';
+    render(
+      <Password password={(password: string) => mockInput = password} />
+    );
+
+    const input = screen.getByRole('textbox') as HTMLInputElement;
+    // input is not at least 6 chars long
+    fireEvent.change(input, {target: {value: 'No$Number'}});
+    expect(mockSetMessage).toHaveBeenCalledTimes(1);
+    expect(mockSetMessage).not.toHaveBeenCalledWith('');
+    expect(mockInput).toBe('');
+  });
+
+  test('should not return value and call setMessage when input is more than 20 chars long', () => {
+    let mockInput = 'NOTTHISVALUE';
+    render(
+      <Password password={(password: string) => mockInput = password} />
+    );
+
+    const input = screen.getByRole('textbox') as HTMLInputElement;
+    // input is not at least 6 chars long
+    fireEvent.change(input, {target: {value: 'ToMuchCharacters!@#$%^&'}});
+    expect(mockSetMessage).toHaveBeenCalledTimes(1);
+    expect(mockSetMessage).not.toHaveBeenCalledWith('');
+    expect(mockInput).toBe('');
+  });
+
+  test('should return value and not call setMessage when input is valid', () => {
+    let mockInput = 'NOTTHISVALUE';
+    render(
+      <Password password={(password: string) => mockInput = password} />
+    );
+
+    const input = screen.getByRole('textbox') as HTMLInputElement;
+    // input is not at least 6 chars long
+    fireEvent.change(input, {target: {value: '0aABc&'}});
+    expect(mockSetMessage).toHaveBeenCalledTimes(1);
+    expect(mockSetMessage).toHaveBeenCalledWith('');
+    expect(mockInput).toBe('0aABc&');
+  });
+
   test('should not return value and call setMessage when input is required', () => {
     let mockInput = 'NOTTHISVALUE';
     render(
@@ -53,7 +152,7 @@ describe('Password component tests', () => {
     fireEvent.change(input, {target: {value: 'MOCKPASSWORD'}});
     fireEvent.change(input, {target: {value: ''}}); // now it is empty
 
-    expect(mockSetMessage).toHaveBeenCalledTimes(1);
+    expect(mockSetMessage).toHaveBeenCalledTimes(2);
     expect(mockSetMessage).not.toHaveBeenCalledWith('');
     expect(mockInput).toBe('');
   });
--- Todo-App/ClientApp/src/shared/inputs/user/password/Password.tsx
+++ Todo-App/ClientApp/src/shared/inputs/user/password/Password.tsx
@@ -6,11 +6,26 @@ const validated = (
   value: string,
   setMessage: (message: string) => void
 ): boolean => {
+  const passwordRegex =
+    new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{6,20})/);
+
+  if (!passwordRegex.test(value)) {
+    const passwordErrorMessage = 'must be a combination of at least:' +
+      ' 1 uppercase letter,' +
+      ' 1 lowercase letter,' +
+      ' 1 of the following characters !@#$%^&* ,' +
+      ' 1 number' +
+      ' and be between 6 and 20 characters long';
+    setMessage(passwordErrorMessage);
+    return false;
+  }
+
   if (!value) {
     setMessage('is required');
     return false;
   }
 
+  setMessage('');
   return true;
 }
 
