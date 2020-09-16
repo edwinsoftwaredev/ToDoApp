7b0cf70b8ef4b285ccc05e38b216854b53421288
Edwin <edwin_m_paz@hotmail.com>
2020-09-15 21:58:05 -0600

    Replace jest.mock for jest.spyOn with import * as ... from '...'

--- Todo-App/ClientApp/src/auth/sign-up/SignUp.test.tsx
+++ Todo-App/ClientApp/src/auth/sign-up/SignUp.test.tsx
@@ -3,33 +3,30 @@ import SignUp, * as SignUpUtils from './SignUp';
 import {render, screen, fireEvent} from '@testing-library/react';
 import {MemoryRouter} from 'react-router-dom';
 import {AuthService} from '../AuthService';
-import Email, {IEmail} from '../../shared/inputs/user/email/Email';
-import Name from '../../shared/inputs/user/name/Name';
-import Username from '../../shared/inputs/user/username/Username';
-import Password from '../../shared/inputs/user/password/Password';
+import * as Email from '../../shared/inputs/user/email/Email';
+import * as Name from '../../shared/inputs/user/name/Name';
+import * as Password from '../../shared/inputs/user/password/Password';
+import * as Username from '../../shared/inputs/user/username/Username';
 
-jest.mock('../../shared/inputs/user/email/Email');
-jest.mock('../../shared/inputs/user/name/Name');
-jest.mock('../../shared/inputs/user/username/Username');
-jest.mock('../../shared/inputs/user/password/Password');
 describe('SignUp Component', () => {
   let spyStartAuthentication: jest.SpyInstance<Promise<void>>;
   const authService: AuthService = AuthService.getInstance();
+  let spyEmail = jest.spyOn(Email, 'default');
+  let spyName = jest.spyOn(Name, 'default');
+  let spyPassword = jest.spyOn(Password, 'default');
+  let spyUsername = jest.spyOn(Username, 'default');
 
   beforeEach(() => {
+    jest.resetModules();
     spyStartAuthentication = jest.spyOn(authService, 'startAuthentication');
-    (Email as jest.Mock).mockImplementation(() => null);
-    (Name as jest.Mock).mockImplementation(() => null);
-    (Username as jest.Mock).mockImplementation(() => null);
-    (Password as jest.Mock).mockImplementation(() => null);
   });
 
   afterEach(() => {
     spyStartAuthentication.mockClear();
-    (Email as jest.Mock).mockClear();
-    (Name as jest.Mock).mockClear();
-    (Username as jest.Mock).mockClear();
-    (Password as jest.Mock).mockClear();
+    spyEmail.mockClear();
+    spyUsername.mockClear();
+    spyName.mockClear();
+    spyPassword.mockClear();
   });
 
   test('should render', () => {
@@ -43,16 +40,16 @@ describe('SignUp Component', () => {
     expect(screen.getByRole('button', {name: 'Sign Up'})).toBeInTheDocument();
   });
 
-  test('should implement User form fields', () => {
+  test('should implement User form fields', async () => {
     render(
       <MemoryRouter>
         <SignUp />
       </MemoryRouter>
     );
 
-    expect(Email as jest.Mock).toHaveBeenCalledTimes(1);
-    expect(Name as jest.Mock).toHaveBeenCalledTimes(1);
-    expect(Username as jest.Mock).toHaveBeenCalledTimes(1);
-    expect(Password as jest.Mock).toHaveBeenCalledTimes(1);
+    expect(spyEmail).toHaveBeenCalledTimes(1);
+    expect(spyName).toHaveBeenCalledTimes(1);
+    expect(spyPassword).toHaveBeenCalledTimes(1);
+    expect(spyUsername).toHaveBeenCalledTimes(1);
   });
 });
