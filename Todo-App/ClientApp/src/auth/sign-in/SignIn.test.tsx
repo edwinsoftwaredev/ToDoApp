df64269313aacfadf69a30e98cbce6f6041e78c5
Edwin <edwin_m_paz@hotmail.com>
2020-09-21 17:13:04 -0600

    Add username and password fields, buttons and handlerSubmit function with a test case to validate the implementation

--- Todo-App/ClientApp/src/auth/sign-in/SignIn.test.tsx
+++ Todo-App/ClientApp/src/auth/sign-in/SignIn.test.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import SignIn, * as SignInUtils from './SignIn';
 import {render} from '@testing-library/react';
 import {MemoryRouter} from 'react-router-dom';
+import * as Username from '../../shared/inputs/user/username/Username';
+import * as Password from '../../shared/inputs/user/password/Password';
+
 
 describe('SignIn Component', () => {
+  let spyUsername: jest.SpyInstance<any>;
+  let spyPassword: jest.SpyInstance<any>;
 
   beforeEach(() => {
+    spyPassword = jest.spyOn(Password, 'default');
+    spyUsername = jest.spyOn(Username, 'default');
   });
 
   afterEach(() => {
+    spyUsername.mockClear();
+    spyPassword.mockClear();
   });
 
   test('should render', () => {
@@ -17,6 +26,27 @@ describe('SignIn Component', () => {
         <SignIn />
       </MemoryRouter>
     );
+  });
+
+  test('should have username and password component fields and 2 buttons', () => {
+    const {container} = render(
+      <MemoryRouter>
+        <SignIn />
+      </MemoryRouter>
+    );
+
+    const inputs =
+      container.getElementsByTagName('input') as HTMLCollectionOf<HTMLInputElement>;
+
+    const buttons =
+      container.getElementsByTagName('button') as HTMLCollectionOf<HTMLButtonElement>;
+
+    expect(inputs.length).toBe(2);
+    // Sign In and Sign Up buttons
+    expect(buttons.length).toBe(2);
+
+    expect(spyUsername).toHaveBeenCalledTimes(1);
+    expect(spyPassword).toHaveBeenCalledTimes(1);
   });
 });
 
--- Todo-App/ClientApp/src/auth/sign-in/SignIn.tsx
+++ Todo-App/ClientApp/src/auth/sign-in/SignIn.tsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './SignIn.scss';
+import Username from '../../shared/inputs/user/username/Username';
+import Password from '../../shared/inputs/user/password/Password';
+import {useHistory} from 'react-router-dom';
+import {AccountService} from '../AccountService';
+import {AxiosError} from 'axios';
+import Message from '../../shared/message/Message';
 
 const SignIn: React.FC = (): JSX.Element => {
+  const [loginData, setLoginData] = useState({});
+  const [disableForm, setDisableForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = (event: any) => {
+    if (!disableForm) {
+      AccountService.loginUser(loginData)
+        .then(() => {
+          // after login this has to something
+        })
+        .catch((error: AxiosError) => {
+          setErrorMessage(error.message);
+        });
+    }
+
+    event.preventDefault();
+  };
+
+  const history = useHistory();
+
+  const handleRoute = () => history.push('/signup');
+
+  useEffect(() => {
+    // here loginData is evaluated to check if it
+    // contains username and password.
+    setDisableForm(
+      !((loginData as any).username
+        && (loginData as any).password
+      ));
+  }, [loginData])
+
   return (
     <div className='signin'>
+      <form className='form' onSubmit={e => disableForm ? null : handleSubmit(e)}>
+        <div className='form-fields'>
+          <Username
+            username={(username: string) => setLoginData({...loginData, username: username})}
+          />
+          <Password
+            password={(password: string) => setLoginData({...loginData, password: password})}
+          />
+        </div>
+        <Message text={errorMessage} />
+        <button
+          className={'uk-button uk-button-default btn-submit' + (disableForm ? '' : ' enabled')}
+          type={disableForm ? 'button' : 'submit'}
+          disabled={disableForm}>
+          Sign Up
+          </button>
+        <button
+          className={'uk-button uk-button-default btn-route-link'}
+          type={'button'}
+          onClick={handleRoute}
+        >
+          Sign In
+          </button>
+      </form>
     </div>
   );
 };
