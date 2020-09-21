800e9c25dc3c76b74b8925a13322d1bc2d8d8ee4
Edwin <edwin_m_paz@hotmail.com>
2020-09-21 17:32:04 -0600

    Add test case - when form is not valid should not submit

--- Todo-App/ClientApp/src/auth/sign-in/SignIn.test.tsx
+++ Todo-App/ClientApp/src/auth/sign-in/SignIn.test.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import SignIn, * as SignInUtils from './SignIn';
-import {render} from '@testing-library/react';
+import {render, fireEvent, screen} from '@testing-library/react';
 import {MemoryRouter} from 'react-router-dom';
 import * as Username from '../../shared/inputs/user/username/Username';
 import * as Password from '../../shared/inputs/user/password/Password';
-
+import {AccountService} from '../../auth/AccountService';
 
 describe('SignIn Component', () => {
   let spyUsername: jest.SpyInstance<any>;
   let spyPassword: jest.SpyInstance<any>;
+  let spyLoginUser: jest.SpyInstance<any>;
 
   beforeEach(() => {
     spyPassword = jest.spyOn(Password, 'default');
     spyUsername = jest.spyOn(Username, 'default');
+    spyLoginUser = jest.spyOn(AccountService, 'loginUser');
   });
 
   afterEach(() => {
     spyUsername.mockClear();
     spyPassword.mockClear();
+    spyLoginUser.mockClear();
   });
 
   test('should render', () => {
@@ -47,6 +50,35 @@ describe('SignIn Component', () => {
 
     expect(spyUsername).toHaveBeenCalledTimes(1);
     expect(spyPassword).toHaveBeenCalledTimes(1);
+  });
+
+  test('should not submit when inputs are not valid', () => {
+    const {container} = render(
+      <MemoryRouter>
+        <SignIn />
+      </MemoryRouter>
+    );
+
+    const inputs =
+      container.getElementsByTagName('input') as HTMLCollectionOf<HTMLInputElement>;
+
+    fireEvent.change(
+      inputs.namedItem('Username') as HTMLInputElement, 'THISISNOTAUSERNAME'
+    );
+
+    fireEvent.change(
+      inputs.namedItem('Password') as HTMLInputElement, 'THISISNOTAPASSWORD'
+    );
+
+    const submitButton = screen.getByText('Sign In') as HTMLButtonElement;
+    const forms = container.getElementsByTagName('form') as HTMLCollectionOf<HTMLFormElement>;
+    fireEvent.submit(forms.item(0) as HTMLFormElement);
+
+    expect(forms.length).toBe(1);
+    expect(submitButton).not.toHaveClass('enabled');
+    expect(submitButton).not.toHaveProperty('type', 'submit');
+    expect(submitButton).toHaveProperty('disabled');
+    expect(spyLoginUser).not.toHaveBeenCalled();
   });
 });
 
