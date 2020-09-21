c7fd82a9ab1f771d96074c4f5825fea21f64bac1
Edwin <edwin_m_paz@hotmail.com>
2020-09-21 17:14:15 -0600

    Add function for user login

--- Todo-App/ClientApp/src/auth/AccountService.tsx
+++ Todo-App/ClientApp/src/auth/AccountService.tsx
@@ -10,6 +10,15 @@ export class AccountService {
   }
 
   /**
+   * Login a user
+   * @return {Promise<void>}
+   */
+  public static loginUser(loginData: any): Promise<void> {
+    return Axios.post(`${process.env.REACT_APP_API_SERVER_URL}api/login`, loginData);
+  }
+
+
+  /**
    * Deletes a user. Only authenticated user are able to delete their own accout.
    */
   public static deleteUser(userName: string): Promise<void> {
