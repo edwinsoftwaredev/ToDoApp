ae89cca463e44250b4d79035fe5eec176566037f
Edwin <edwin_m_paz@hotmail.com>
2020-09-22 18:13:17 -0600

    Add interface to be implemented by AuthenticationService class

---
+++ Todo-App/Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Todo_App.Model.Auth.VM;
+
+namespace Todo_App.Services.Interfaces {
+    public interface IAuthenticationService {
+        public Task<SignInResult> Authenticate(LoginData loginData);
+    }
+}
