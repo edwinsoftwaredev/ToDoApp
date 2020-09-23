d14cd43a0b35389223999b1f6aadf5975bd30365
Edwin <edwin_m_paz@hotmail.com>
2020-09-22 18:09:20 -0600

    Add AuthenticationService and unit test cases for it

---
+++ Todo-App/Services/AuthenticationService.cs
@@ -0,0 +1,50 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Todo_App.Model.Auth;
+using Todo_App.Model.Auth.VM;
+using Todo_App.Services.Interfaces;
+
+namespace Todo_App.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        readonly SignInManager<User> _signInManager;
+        readonly ILogger<AuthenticationService> _logger;
+        readonly IHttpContextAccessor _httpContextAcessor;
+
+        public AuthenticationService(
+                SignInManager<User> signInManager,
+                ILogger<AuthenticationService> logger,
+                IHttpContextAccessor httpContextAccessor
+            )
+        {
+            this._signInManager = signInManager;
+            this._logger = logger;
+            this._httpContextAcessor = httpContextAccessor;
+        }
+
+        // https://docs.microsoft.com/en-us/aspnet/core/security/authentication/identity
+        public async Task<SignInResult> Authenticate(LoginData loginData) {
+            if (this._signInManager.IsSignedIn(this._httpContextAcessor.HttpContext.User)) {
+                return SignInResult.Failed;
+            }
+
+            var result = await this._signInManager
+                .PasswordSignInAsync(
+                        loginData.Username,
+                        loginData.Password,
+                        true,
+                        true
+                    );
+
+            if (result.Succeeded) {
+                _logger.LogInformation("User logged in.");
+                return result;
+            }
+
+            return SignInResult.Failed;
+        }
+    }
+}
