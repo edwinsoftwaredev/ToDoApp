3c23b879d9d89757b4cb7fcdcd5622768fb02994
Edwin <edwin_m_paz@hotmail.com>
2020-09-25 18:55:29 -0600

    Add functionality to add an initial client in startup time or migration

--- Todo-App/DAL/DataSeedingContext.cs
+++ Todo-App/DAL/DataSeedingContext.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using IdentityServer4;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Todo_App.Model.Auth;
@@ -10,25 +15,72 @@ namespace Todo_App.DAL
     public static class DataSeedingContext
     {
         public static void Initialize(IdDbContext context,
-                RoleManager<Role> roleManager)
+                RoleManager<Role> roleManager,
+                ConfigurationDbContext configurationDbContext)
         {
+
+            /**
+             * How to:
+             * First generate the migration IF THE IS A CHANGE IN THE MODEL,
+             * if what it needs it just seeding the database generate a new migration
+             * is not something to do.
+             *
+             * Second just start the server. That will migrate the database if it is needed
+             * and will seed the database.
+             *
+             * in production with many instances running this type of code can cause
+             * problems, Read 'apply-migrations-at-runtime':
+             * https://docs.microsoft.com/en-us/ef/core/managing-schemas/migrations/applying
+             *
+             * THIS IS A MIGRATION AT RUNTIME
+             **/
+
             // use code with precaution
             context.Database.Migrate();
 
-            if (context.Roles.Any())
+            if (!context.Roles.Any())
             {
-                return; // database has been seeded
+                // seeding database
+                var result = roleManager.CreateAsync(new Role
+                       {
+                          Name = RoleConstants.USER_ROLE,
+                          NormalizedName = RoleConstants.USER_ROLE
+                       }).Result;
+
+                if (!result.Succeeded)
+                    throw new Exception("An Error occurred when creating the DB");
             }
 
-            // seeding database
-            var result = roleManager.CreateAsync(new Role
-                   {
-                      Name = RoleConstants.USER_ROLE,
-                      NormalizedName = RoleConstants.USER_ROLE
-                   }).Result;
+            if (!configurationDbContext.Clients.Any())
+            {
+                var firstPartyUserClient = new IdentityServer4.Models.Client
+                {
+                    ClientId = "TodoAppFirstPartyUser",
+                    ClientName = "Todo App First Party - User",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false, // default is true
+                    RequirePkce = true, // default is false
+                    AllowPlainTextPkce = false, // default is false
+                    RequireConsent = false, // this is a firstParty client
+                    AllowedScopes =
+                    {
+                        "TodoAppApi.TodoAppUser", // This is name of an ApiResource scope
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    RedirectUris = new List<string>
+                    {
+                        "https://localhost:5001/auth/codes"
+                    },
+                    AllowedCorsOrigins = new List<string>
+                    {
+                        "https://localhost:5001"
+                    }
+                }.ToEntity();
 
-            if (!result.Succeeded)
-                throw new Exception("An Error occurred when creating the DB");
+                configurationDbContext.Clients.Add(firstPartyUserClient);
+                configurationDbContext.SaveChanges();
+            }
 
             return;
         }
