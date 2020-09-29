8150667fade97bbb66efc46fe198dae362d44959
Edwin <edwin_m_paz@hotmail.com>
2020-09-28 18:17:12 -0600

    Move getUser() sideeffect to useEffect hook

--- Todo-App/ClientApp/src/App.tsx
+++ Todo-App/ClientApp/src/App.tsx
@@ -1,37 +1,42 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.scss';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  useLocation
+  useLocation,
 } from 'react-router-dom';
 import Home from './home/Home';
 import Auth from './auth/Auth';
 import {AuthService} from './auth/AuthService';
-import {TestComponent} from './test-component/TestComponent';
 import {User as OidcUser} from 'oidc-client';
 import {saveUser} from './auth/auth-codes/AuthCodes';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 
 function AppContainer(): JSX.Element {
   const authService = AuthService.getInstance();
   const PrivateRoute = authService.privateRoute;
   const dispatch = useDispatch();
-
   // this hook cant be called inside a component
   // that implements Router
   const location = useLocation();
+  const isUserLoggedIn = useSelector(AuthService.isUserLoggedInSelector);
 
-  authService.getUser().then((user: OidcUser | null) => {
-    if (user) {
-      dispatch(saveUser(user));
-    } else {
-      if (location.pathname !== '/signin' && location.pathname !== '/auth/codes') {
-        authService.startAuthentication();
-      }
+  useEffect(() => {
+    if (
+      location.pathname !== '/signin' &&
+      location.pathname !== '/auth/codes' &&
+      !isUserLoggedIn
+    ) {
+      authService.getUser().then((user: OidcUser | null) => {
+        if (user) {
+          dispatch(saveUser(user));
+        } else {
+          authService.startAuthentication();
+        }
+      });
     }
-  });
+  }, [authService, dispatch, isUserLoggedIn, location.pathname]);
+
   return (
     <div className="App">
       <Switch>
