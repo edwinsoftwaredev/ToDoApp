42b963171687d05058c6d7348a80fbdfb7d34f8e
Edwin <edwin_m_paz@hotmail.com>
2020-09-28 18:19:51 -0600

    Move completeAuthentication() to useEffect hook

--- Todo-App/ClientApp/src/auth/auth-codes/AuthCodes.tsx
+++ Todo-App/ClientApp/src/auth/auth-codes/AuthCodes.tsx
@@ -3,12 +3,12 @@ import {AuthService} from '../AuthService';
 import {User as OidcUser} from 'oidc-client';
 import Message from '../../shared/message/Message';
 import {AxiosError} from 'axios';
-import {useHistory} from 'react-router-dom';
 import {ActionsObservable, ofType, StateObservable} from 'redux-observable';
 import {Action} from '@reduxjs/toolkit';
 import {map} from 'rxjs/operators';
-import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import {RootState} from '../../reducers/RootReducer';
+import {useHistory} from 'react-router-dom';
 
 enum ActionTypes {
   SAVE_USER = 'oidcUser/identifyUser', // this is the name of the Action in the slice
@@ -36,29 +36,21 @@ export type SaveUserActions = ISaveUser;
 const AuthCodes: React.FC = () => {
   const [errorMessage, setErrorMessage] = useState('');
 
-  const history = useHistory();
-  // here this is redirecting to home -> '/'
-  // but it has to be a previous route before authentication page
-  // that route has to be saved and retrived in the following function.
-  const handlePreviousRoute = () => history.push('/');
   const authService = AuthService.getInstance();
   const dispatch = useDispatch();
-
-  const isUserLoggedIn = useSelector(AuthService.isUserLoggedInSelector);
-  authService.completeAuthentication().then((user: OidcUser | void) => {
-    if (user) {
-      setErrorMessage('');
-      dispatch(saveUser(user));
-    }
-  }).catch((error: AxiosError) => {
-    setErrorMessage(error.message);
-  });
+  const history = useHistory();
 
   useEffect(() => {
-    if (isUserLoggedIn) {
-      handlePreviousRoute();
-    }
-  }, [handlePreviousRoute, isUserLoggedIn]);
+    authService.completeAuthentication().then((user: OidcUser | void) => {
+      if (user) {
+        setErrorMessage('');
+        dispatch(saveUser(user));
+        history.push('/') // <-- push to Home component
+      }
+    }).catch((error: AxiosError) => {
+      setErrorMessage(error.message);
+    });
+  }, [authService, dispatch, history]);
 
   return (
     <Message text={errorMessage} />
