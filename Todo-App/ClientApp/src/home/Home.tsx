54ba0e11a393e549e1fbed44b22c300fee8bde43
Edwin <edwin_m_paz@hotmail.com>
2020-09-30 17:00:58 -0600

    Add button to handle signouts

--- Todo-App/ClientApp/src/home/Home.tsx
+++ Todo-App/ClientApp/src/home/Home.tsx
@@ -2,11 +2,32 @@ import React from 'react';
 import logo from '../logo.svg';
 import {useSelector} from 'react-redux';
 import {RootState} from '../reducers/RootReducer';
+import './Home.scss';
+import {AuthService} from '../auth/AuthService';
+import {AccountService} from '../auth/AccountService';
+
+/**
+ * this component contains the code requiered to logout,
+ * that doesn't mean it has to be placed here.
+ */
 
 const Home: React.FC = (): JSX.Element => {
+  const authService = AuthService.getInstance();
+
+  const handleLogout = () => {
+    // first delete auth cookie, the startSignOut from IdentityServer
+    AccountService.deleteAuthCookie().then(() => {
+      authService.startSignOut();
+    });
+  };
+
   return (
-    <div>
-      <h2>{useSelector((state: RootState) => state.app.appName)}</h2>
+    <div className='home-component'>
+      <header>
+        <h1 className='title'>
+          {useSelector((state: RootState) => state.app.appName)}
+        </h1>
+      </header>
       <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
@@ -19,6 +40,12 @@ const Home: React.FC = (): JSX.Element => {
       >
         Learn React
       </a>
+      <br />
+      <button
+        className={'uk-button uk-button-default'}
+        onClick={handleLogout}>
+        Sign Out
+      </button>
     </div>
   );
 }
