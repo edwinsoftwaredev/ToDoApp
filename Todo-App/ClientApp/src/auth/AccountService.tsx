e75341cfaac88558036e8a46b97071da44ca4281
Edwin <edwin_m_paz@hotmail.com>
2020-09-30 16:58:26 -0600

    Add method to get antiforgery tokens and add an interceptor for request to add the x-xsrf-token in header. Also Add method to delete authentication cookie as an initial step to signout

--- Todo-App/ClientApp/src/auth/AccountService.tsx
+++ Todo-App/ClientApp/src/auth/AccountService.tsx
@@ -1,12 +1,19 @@
-import Axios, {AxiosResponse} from 'axios';
+import Axios, {AxiosResponse, AxiosRequestConfig} from 'axios';
 
 export class AccountService {
+
+  private static _xsrfInterceptorId: number;
+
   /**
    * Registers a new user
    * @return {Promise<void>}
    */
   public static registerUser(userObj: object): Promise<void> {
-    return Axios.post(`${process.env.REACT_APP_API_SERVER_URL}/api/user`, userObj);
+
+    return Axios.post(
+      `${process.env.REACT_APP_API_SERVER_URL}/api/user`,
+      userObj
+    );
   }
 
   /**
@@ -14,14 +21,70 @@ export class AccountService {
    * @return {Promise<void>}
    */
   public static authenticateUser(loginData: any): Promise<AxiosResponse<any>> {
-    return Axios.post(`${process.env.REACT_APP_API_SERVER_URL}/api/authentication`, loginData);
+    return Axios.post(
+      `${process.env.REACT_APP_API_SERVER_URL}/api/authentication/signin`,
+      loginData
+    );
   }
 
+  /**
+   * makes a request to delete authentication cookie
+   * @returns {Promise<void>}
+   */
+  public static deleteAuthCookie(): Promise<void> {
+    return Axios.post(
+      `${process.env.REACT_APP_API_SERVER_URL}/api/authentication/signout`
+    );
+  }
 
   /**
    * Deletes a user. Only authenticated user are able to delete their own accout.
    */
   public static deleteUser(userName: string): Promise<void> {
-    return Axios.delete(`${process.env.REACT_APP_API_SERVER_URL}/api/user?id=${userName}`);
+    return Axios.delete(
+      `${process.env.REACT_APP_API_SERVER_URL}/api/user?id=${userName}`
+    );
+  }
+
+  /**
+   * Makes a request to get csrf token and add an interceptor to add
+   * a X-XSRF-TOKEN on every request
+   */
+  public static async getAntiForgeryToken(): Promise<void> {
+    return Axios.get(`${process.env.REACT_APP_API_SERVER_URL}/api/xsrftoken`)
+      .then((response: AxiosResponse) => {
+
+        const headerName = response.config.xsrfHeaderName;
+
+        // here this will get the xcsrfCookie
+        if (headerName) {
+          const xcsrfCookie = document
+            .cookie
+            .split('; ')
+            .find(cookie => cookie.startsWith(headerName))
+            ?.split('=')[1];
+
+          if (xcsrfCookie) {
+            // here this will set the cookie
+            window.sessionStorage.setItem(headerName, xcsrfCookie);
+            window.sessionStorage.setItem('csrf-token-headername', headerName);
+          }
+        }
+
+        Axios.interceptors.request.eject(this._xsrfInterceptorId);
+
+        this._xsrfInterceptorId = Axios.interceptors.request.use((request: AxiosRequestConfig) => {
+          const xcsrfHeaderName = window.sessionStorage.getItem('csrf-token-headername');
+
+          if (!xcsrfHeaderName) return request;
+
+          const xcsrfToken = window.sessionStorage.getItem(xcsrfHeaderName);
+
+          if (!xcsrfToken) return request;
+
+          request.headers[xcsrfHeaderName] = xcsrfToken;
+          return request;
+        });
+      });
   }
 }
