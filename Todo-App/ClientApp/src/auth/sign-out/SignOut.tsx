1847c67842e3c586f0d013ac261a43224b8f94d8
Edwin <edwin_m_paz@hotmail.com>
2020-09-30 16:33:47 -0600

    Add SignOut Component

---
+++ Todo-App/ClientApp/src/auth/sign-out/SignOut.tsx
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import {AuthService} from '../AuthService';
+import Message from '../../shared/message/Message';
+import {useHistory} from 'react-router-dom';
+import {AxiosError} from 'axios';
+
+const SignOut: React.FC = () => {
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const authService = AuthService.getInstance();
+  const history = useHistory();
+
+  useEffect(() => {
+    authService.completeSignOut().then(() => {
+      // history.push('/'); // should redirects to other route
+    }).catch((error: AxiosError) => {
+      setErrorMessage(error.message);
+    });
+  }, [authService, history])
+
+  return (
+    <div>
+      <h1>Good Bye!</h1>
+      <Message text={errorMessage} />
+    </div>
+  );
+}
+
+export default SignOut;
