3b8766492b940c609fdf62a951ef3836573ea016
Edwin <edwin_m_paz@hotmail.com>
2020-10-08 15:13:15 -0600

    Remove logic for the weather widget

--- Todo-App/ClientApp/src/home/Home.tsx
+++ Todo-App/ClientApp/src/home/Home.tsx
@@ -5,11 +5,8 @@ import {RootState} from '../reducers/RootReducer';
 import './Home.scss';
 import {AuthService} from '../auth/AuthService';
 import {AccountService} from '../auth/AccountService';
-import Axios, {AxiosResponse, AxiosError} from 'axios';
-/**
- * this component contains the code requiered to logout,
- * that doesn't mean it has to be placed here.
- */
+import WeatherWidget from './weather-widget/WeatherWidget';
+
 const MenuList = (props: any): JSX.Element => {
   const [prevTarget, setPrevTarget] = useState<HTMLLIElement>();
   const [selectedTarget, setSelectedTarget] = useState();
@@ -35,7 +32,7 @@ const MenuList = (props: any): JSX.Element => {
   }, [prevTarget, selectedTarget]);
 
   return (
-    <nav >
+    <nav className={props.screenWidth < 767 ? ' smart-screen-list' : ''}>
       <ol>
         <li
           onClick={event => handleTargetSelection(event.currentTarget, 'feature')}
@@ -66,149 +63,31 @@ const MenuList = (props: any): JSX.Element => {
   );
 };
 
-const SideMenu = (): JSX.Element => {
-  const [weatherDesc, setWeatherDesc] = useState<string>('');
-  const [weatherTemp, setWeatherTemp] = useState<string>('0');
-  const [weatherBgClass, setWeatherBgClass] = useState<string>('');
-
-  const convertWeatherDesc = (response: AxiosResponse<any>) => {
-    let wbgclass = '';
-    let description = response.data.weather[0].description;
-    const rain: string[] = [
-      'light rain',
-      'moderate rain',
-      'heavy intensity rain',
-      'very heavy rain',
-      'extreme rain',
-      'freezing rain',
-      'light intensity shower rain',
-      'heavy intensity shower rain',
-      'ragged shower rain'
-    ];
-
-    const snow: string[] = [
-      'light snow',
-      'Heavy snow',
-      'Sleet',
-      'Light shower sleet',
-      'Shower sleet',
-      'Light rain and snow',
-      'Rain and snow',
-      'Light shower snow',
-      'Shower snow',
-      'Heavy shower snow'
-    ];
-
-    const atmosphere: string[] = [
-      'Smoke',
-      'Haze',
-      'sand/ dust whirls',
-      'fog',
-      'sand',
-      'dust',
-      'volcanic ash',
-      'squalls',
-      'tornado'
-    ];
-
-    const clouds: string[] = [
-      'few clouds',
-      'overcast clouds'
-    ];
-
-    if (rain.includes(description)) {
-      description = 'rain';
-    }
-
-    if (snow.includes(description)) {
-      description = 'snow';
-    }
-
-    if (atmosphere.includes(description)) {
-      description = 'mist';
-    }
-
-    if (clouds.includes(description)) {
-      description = 'scattered clouds';
-    }
-
-    wbgclass = description.replace(' ', '-');
-    wbgclass =
-      response
-        .data
-        .weather[0]
-        .icon
-        .includes('d') ? wbgclass + '-d' : wbgclass + '-n';
-    setWeatherBgClass(wbgclass);
-  };
-
-  navigator.geolocation.getCurrentPosition((position: Position) => {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const apiUrl = process.env.REACT_APP_API_WEATHER_KEY;
-
-    if (!weatherDesc) {
-      Axios
-        .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiUrl}`)
-        .then((response: AxiosResponse) => {
-          setWeatherDesc(response.data.weather[0].description);
-          setWeatherTemp(response.data.main.temp);
-          convertWeatherDesc(response);
-        }).catch((error: AxiosError) => {
-          console.log(error.message);
-        });
-    }
-
-  }, ((posError: PositionError) => {
-    console.log(posError);
-  }), {enableHighAccuracy: true} as PositionOptions);
-
+const Menu = (props: any): JSX.Element => {
   const handleRoute = (route: string) => {
     console.log(route);
   };
 
-  const date = new Date().toString().split(' ');
-  let actualDate = date[2];
-  let grade = 'th';
-  if (actualDate.includes('1', 1)) {
-    grade = 'st';
-  }
-  if (actualDate.includes('1', 1)) {
-    grade = 'nd';
-  }
-  if (actualDate.includes('1', 1)) {
-    grade = 'rd';
-  }
-  actualDate = (actualDate.includes('0', 0) ? actualDate[1] : actualDate);
-
   return (
     <div className={'SideMenu'}>
       <section className='side-menu-section'>
-        <div className='date-picture-weather-container'>
-          <div className='date-picture-weather'>
-            <div className={'widgets-container ' + weatherBgClass}>
-              <div className='day-date'>
-                {date[0] + ', ' + actualDate + grade}
-              </div>
-              <div className='month-year'>
-                {date[1] + ', ' + date[3]}
-              </div>
-              <div className='weather-status'>
-                {weatherDesc}
-              </div>
-              <div className='weather-stat'>
-                {Math.round(Number.parseFloat(weatherTemp as string))}F°
-              </div>
-            </div>
-          </div>
+        <div className={'smart-screen-weather-widget'}>
+          <WeatherWidget />
         </div>
-        <MenuList handleRoute={handleRoute} />
+        <MenuList screenWidth={props.screenWidth} handleRoute={handleRoute} />
       </section>
     </div>
   );
 };
 
 const Home: React.FC = (): JSX.Element => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const resizeListener = () => {
+    setWidth(window.innerWidth);
+  };
+
+  window.addEventListener('resize', resizeListener);
 
   const authService = AuthService.getInstance();
 
@@ -221,7 +100,7 @@ const Home: React.FC = (): JSX.Element => {
 
   return (
     <div className={'Home'}>
-      <SideMenu />
+      <Menu screenWidth={width} />
       <div className='container'>
         <main className='home-main'>
           <header>
