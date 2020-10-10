01727fcc9086540baeba50d0b040c1d12ef2cefe
Edwin <edwin_m_paz@hotmail.com>
2020-10-10 12:44:14 -0600

    Add Switch for routing between paths

--- Todo-App/ClientApp/src/home/Home.tsx
+++ Todo-App/ClientApp/src/home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.scss';
 import Menu from './menu/Menu';
 import FeatureSection from './feature-section/FeatureSection';
+import {Switch, Route} from 'react-router-dom';
 
 const Home: React.FC = (): JSX.Element => {
   return (
@@ -9,16 +10,26 @@ const Home: React.FC = (): JSX.Element => {
       <Menu />
       <div className='container'>
         <main className='home-main'>
-          <section className='feature-section'>
-            <div className='feature-section-inner'>
-              <FeatureSection />
-            </div>
-          </section>
-          <section className='today-todos-section'>
-            <div className='today-todos-section-inner'>
-              All todos
-            </div>
-          </section>
+          <Switch>
+            <Route exact path="/">
+              <section className='feature-section'>
+                <div className='feature-section-inner'>
+                  <FeatureSection />
+                </div>
+              </section>
+              <section className='today-todos-section'>
+                <div className='today-todos-section-inner'>
+                  All todos
+                </div>
+              </section>
+            </Route>
+            <Route exact path="/calendar">
+              <div className='calendar-container'></div>
+            </Route>
+            <Route exact path="/account">
+              <div className='account-container'></div>
+            </Route>
+          </Switch>
         </main>
       </div>
     </div>
