a23a0a5fcfe3e518500a2c9a102ef69560b9bf06
Edwin <edwin_m_paz@hotmail.com>
2020-10-14 18:53:37 -0600

    Rename class

--- Todo-App/Model/TodoRest/TodoUser.cs
+++ Todo-App/Model/TodoRest/TodoUser.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using Todo_App.Model.Auth;
 
 namespace Todo_App.Model.TodoRest {
-    public class UserTodo : User {
+    public class TodoUser : User {
         public List<Todo> TodoList { get; set; }
     }
 }
