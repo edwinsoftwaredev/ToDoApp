f29289a7791e5e551d25f16db8f38bc057cc7f23
Edwin <edwin_m_paz@hotmail.com>
2020-10-15 19:29:44 -0600

    Add migrations to update Todo entity

---
+++ Todo-App/Migrations/20201015175556_FixTodoColumnNameFeature.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Todo_App.Migrations
+{
+    public partial class FixTodoColumnNameFeature : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFetured",
+                table: "Todo");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFeatured",
+                table: "Todo",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFeatured",
+                table: "Todo");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFetured",
+                table: "Todo",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
---
+++ Todo-App/Migrations/20201015220826_ReplaceTodosPropertyNameChecked.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Todo_App.Migrations
+{
+    public partial class ReplaceTodosPropertyNameChecked : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cheked",
+                table: "Todo");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "Todo",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "Todo");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Cheked",
+                table: "Todo",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
