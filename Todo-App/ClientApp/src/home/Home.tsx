5fd3d1a8eda0abaa53acf95700fe0dd3d27de080
Edwin <edwin_m_paz@hotmail.com>
2020-10-23 18:06:56 -0600

    Add useEffect to load weather data and get TodoUser

--- Todo-App/ClientApp/src/home/Home.tsx
+++ Todo-App/ClientApp/src/home/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Home.scss';
 import Menu from './menu/Menu';
 import {Switch, Route} from 'react-router-dom';
@@ -8,29 +8,45 @@ import Axios, {AxiosError, AxiosResponse} from 'axios';
 import {useDispatch} from 'react-redux';
 import {saveWeather} from './weather-widget/WeatherWidget';
 import CompletedTodos from './completed-todos/CompletedTodos';
+import {AuthService} from '../auth/AuthService';
 
 const Home: React.FC = (): JSX.Element => {
   const dispatch = useDispatch();
 
-  // getting weather in startup
-  navigator.geolocation.getCurrentPosition((position: Position) => {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const apiKey = process.env.REACT_APP_API_WEATHER_KEY;
-    Axios
-      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
-      .then((response: AxiosResponse) => {
-        dispatch(saveWeather({
-          weatherDescription: response.data.weather[0].description,
-          weatherTemperature: response.data.main.temp,
-          weatherIcon: response.data.weather[0].icon
-        }));
-      }).catch((error: AxiosError) => {
-        console.log(error.message);
-      });
-  }, ((posError: PositionError) => {
-    console.log(posError);
-  }), {enableHighAccuracy: true} as PositionOptions);
+  // getting weather at startup
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position: Position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      const apiKey = process.env.REACT_APP_API_WEATHER_KEY;
+      Axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
+        .then((response: AxiosResponse) => {
+          dispatch(saveWeather({
+            weatherDescription: response.data.weather[0].description,
+            weatherTemperature: response.data.main.temp,
+            weatherIcon: response.data.weather[0].icon
+          }));
+        }).catch((error: AxiosError) => {
+          console.log(error.message);
+        });
+    }, ((posError: PositionError) => {
+      console.log(posError);
+    }), {enableHighAccuracy: true} as PositionOptions);
+  }, [dispatch]);
+
+  // getting/setting TodoUser at startup
+  const getTodoUser = async () => {
+    const user = await AuthService.getInstance().getUser();
+    Axios.get(
+      `${process.env.REACT_APP_TODO_SERVER_URL}/api/todouser`
+      , {headers: {'Authorization': `${user?.token_type} ${user?.access_token}`}}
+    );
+  };
+
+  useEffect(() => {
+    getTodoUser();
+  }, [])
 
   return (
     <div className={'Home'}>
