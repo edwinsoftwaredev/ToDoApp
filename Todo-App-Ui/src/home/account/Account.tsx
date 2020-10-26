b7812d74d44ab7803ea342035b42e19f58111b11
Edwin <edwin_m_paz@hotmail.com>
2020-10-26 17:57:09 -0600

    Add Account component

---
+++ Todo-App-Ui/src/home/account/Account.tsx
@@ -0,0 +1,38 @@
+﻿import React from 'react';
+import './Account.scss';
+import {useSelector} from "react-redux";
+import {todoUserSelector} from "../Home";
+import Username from "../../shared/inputs/user/username/Username";
+
+const Account: React.FC = () => {
+  const todoUser = useSelector(todoUserSelector);
+  
+  const handleDeleteAccount = () => {
+  }
+  
+  return (
+    <div className='Account'>
+      <h1 className='heading'>My Account</h1>
+      <div className='account-container'>
+        <div className='account-info'>
+          <div className={'account-username'}>
+            <Username
+              username={() => null}
+              initialValue={todoUser.userName}
+              isDisabled={true}
+            />
+          </div>
+          <div></div>
+          <button
+            className={'uk-button uk-button-default btn-delete-account'}
+            onClick={handleDeleteAccount}
+          >
+            Delete Account
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Account;
