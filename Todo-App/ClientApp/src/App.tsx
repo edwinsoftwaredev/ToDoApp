fc68200f6f60e17309756e37ee3b2ecfa0e85f2a
Edwin <edwin_m_paz@hotmail.com>
2020-10-25 22:59:36 -0600

    Remove code not required by auth server client app

--- Todo-App/ClientApp/src/App.tsx
+++ Todo-App/ClientApp/src/App.tsx
@@ -3,46 +3,16 @@ import './App.scss';
 import {
   BrowserRouter as Router,
 } from 'react-router-dom';
-import Home from './home/Home';
 import Auth from './auth/Auth';
-import {AuthService} from './auth/AuthService';
-import {User as OidcUser} from 'oidc-client';
-import {saveUser} from './auth/auth-codes/AuthCodes';
-import {useDispatch, useSelector} from 'react-redux';
-import {AccountService} from './auth/AccountService';
 
 function AppContainer(): JSX.Element {
-  const authService = AuthService.getInstance();
-  const dispatch = useDispatch();
-  // this hook cant be called inside a component
-  // that implements Router
-  const isUserLoggedIn = useSelector(AuthService.isUserLoggedInSelector);
-
   useEffect(() => {
-    AccountService.getAntiForgeryToken().then(() => {
-      const windowLocation = window.location.pathname;
-      if (
-        !isUserLoggedIn &&
-        windowLocation !== '/authentication/signin' &&
-        windowLocation !== '/authentication/signup' &&
-        windowLocation !== '/authentication/signout' &&
-        windowLocation !== '/auth/codes'
-      ) {
-        authService.getUser().then((user: OidcUser | null) => {
-          if (user) {
-            dispatch(saveUser(user));
-          } else {
-            authService.startAuthentication(); // this generates a browser redirection
-          }
-        });
-      }
-    });
-  }, [authService, dispatch, isUserLoggedIn]);
+    // AccountService.getAntiForgeryToken();
+  }, []);
 
   return (
     <div className="App">
-      {!isUserLoggedIn ? <Auth /> : null}
-      {isUserLoggedIn ? <Home /> : null}
+      <Auth />
     </div>
   );
 }
