bfe3757760e1783b4ed8c8a56ee122ddfc9fb3b2
Edwin <edwin_m_paz@hotmail.com>
2020-10-27 18:50:07 -0600

    Add Account service

---
+++ Todo-App-Ui/src/home/account/AccountService.tsx
@@ -0,0 +1,14 @@
+﻿import Axios, {AxiosResponse} from "axios";
+
+export default class AccountService {
+  public static deleteTodoAccount(id: string): Promise<AxiosResponse<void>> {
+    return Axios.delete(`${process.env.REACT_APP_TODO_SERVER_ULR}/api/todouser?id=${id}`);
+  }
+  
+  /* this is not ideal. The ideal solution is having, in the authentication server,
+  * a component to let users to manage their own account.
+  */ 
+  public static deleteAccount(id: string): Promise<AxiosResponse<void>> {
+    return Axios.delete(`${process.env.REACT_APP_AUTH_SERVER_URL}/api/user?id=${id}`);
+  }
+}
