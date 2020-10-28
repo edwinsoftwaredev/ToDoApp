07ea2ba39cfab7e09986cfef424147ad27f5b562
Edwin <edwin_m_paz@hotmail.com>
2020-10-28 14:04:50 -0600

    Add authorization token and fix path

--- Todo-App-Ui/src/home/account/AccountService.tsx
+++ Todo-App-Ui/src/home/account/AccountService.tsx
@@ -1,14 +1,20 @@
 ﻿import Axios, {AxiosResponse} from "axios";
+import {AuthService} from "../../auth/AuthService";
 
 export default class AccountService {
-  public static deleteTodoAccount(id: string): Promise<AxiosResponse<void>> {
-    return Axios.delete(`${process.env.REACT_APP_TODO_SERVER_ULR}/api/todouser?id=${id}`);
+  private static readonly authService = AuthService.getInstance();
+  
+  public static async deleteTodoAccount(id: string): Promise<AxiosResponse<void>> {
+    const user = await this.authService.getUser();
+    return Axios.delete(`${process.env.REACT_APP_TODO_SERVER_URL}/api/todouser/${id}`, {
+      headers: {'Authorization': `${user?.token_type} ${user?.access_token}`}
+    });
   }
   
   /* this is not ideal. The ideal solution is having, in the authentication server,
   * a component to let users to manage their own account.
   */ 
   public static deleteAccount(id: string): Promise<AxiosResponse<void>> {
-    return Axios.delete(`${process.env.REACT_APP_AUTH_SERVER_URL}/api/user?id=${id}`);
+    return Axios.delete(`${process.env.REACT_APP_AUTH_SERVER_URL}/api/user/${id}`);
   }
 }
