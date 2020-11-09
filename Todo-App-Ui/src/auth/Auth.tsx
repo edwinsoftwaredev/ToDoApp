7c36aed34617fe8503e2573a13469c1c72be0436
Edwin <edwin_m_paz@hotmail.com>
2020-11-09 15:05:24 -0600

    Add functionality to check if authentication server is alive

--- Todo-App-Ui/src/auth/Auth.tsx
+++ Todo-App-Ui/src/auth/Auth.tsx
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {Switch, Route, useLocation} from 'react-router-dom';
 import {AuthService} from './AuthService';
 import './Auth.scss';
 import AuthCodes from './auth-codes/AuthCodes';
+import Axios, {AxiosResponse} from 'axios';
 
 const Auth: React.FC = (): JSX.Element => {
   const authService = AuthService.getInstance();
   const location = useLocation();
   const isAuthCodesPath = location.pathname !== "/auth/codes";
+  const [authServerAlive, setAuthServerAlive] = useState(false);
+  const intervalId = useRef<number>(0);
 
   const handleAuthPathRedirection = (): JSX.Element => {
-    authService.startAuthentication();
     return AuthService.NotAuthenticated();
   }
+
+  const checkIfAlive = () => {
+    Axios.get(`${process.env.REACT_APP_AUTH_SERVER_URL}/.well-known/openid-configuration`)
+      .then((response: AxiosResponse) => {
+        if (response.status === 200) {
+          setAuthServerAlive(true);
+        }
+      });
+  };
+
+  useEffect(() => {
+    if (!authServerAlive) {
+      intervalId.current = window.setInterval(checkIfAlive, 1000);
+    }
+
+    return () => {
+      window.clearInterval(intervalId.current);
+    };
+  }, [authServerAlive]);
+
+  useEffect(() => {
+    if (authServerAlive) {
+      window.clearInterval(intervalId.current);
+      authService.startAuthentication();
+    }
+
+    return () => {
+      window.clearInterval(intervalId.current);
+    };
+  }, [authServerAlive, authService]);
 
   return (
     <div className='Auth'>
